09df285c16ea0812ed3f1e5308bcc318d02d0550
Dhruv Tyagi <dhruvtyagi01@gmail.com>
2023-05-27 23:31:13 +0530

    .

---
+++ MTE34.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract A {
+    function f1() public pure returns (uint) {
+        return 1;
+    }
+
+    function f2() private pure returns (uint) {
+        return 2;
+    }
+
+    function f3() internal pure returns (uint) {
+        uint256 xv = f3();
+        return 3;
+    }
+
+    function f4() external pure returns (uint) {
+        return 4;
+    }
+}
